222dae0eab35ba33c7c3d989919e58cec5dc962b
Pawel <p.j.rusnak97@gmail.com>
2025-07-14 18:30:46 +0200

    env

--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -7,7 +7,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
+    PORT: Joi.number().default(3005),
     POSTGRES_URL: Joi.string().required().description('Postgres DB url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
