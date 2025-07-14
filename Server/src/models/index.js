4f79def3c56f9042b276460ba2afb1457d94dace
Pawel <p.j.rusnak97@gmail.com>
2025-07-14 17:46:19 +0200

    rm mongo

--- Server/src/models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Comment = require('./comment.model');
-module.exports.Game = require('./game.model');
-module.exports.Rating = require('./rating.model');
-module.exports.Score = require('./score.model');
