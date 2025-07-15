491265fa2440df2ab05c093c68c914e687392422
Pawel <p.j.rusnak97@gmail.com>
2025-07-15 15:29:16 +0200

    defaulkt

--- Frontend/src/Components/GradeDropdown/index.jsx
+++ Frontend/src/Components/GradeDropdown/index.jsx
@@ -1,31 +1,53 @@
-import { MenuItem, Select } from '@mui/material'
-import React from 'react'
-import styled from 'styled-components'
-import grades from '../../Assets/Grades'
+import { MenuItem, Select } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
+import grades from "../../Assets/Grades";
 
 const GradeDropdown = ({ value, label, onChange }) => {
-    return <Select
-        value={value}
-        label={label}
-        onChange={onChange}
-    >
-        <MenuItem value={'SSS'}><Grade src={grades.SSS}/></MenuItem>
-        <MenuItem value={'SS'}><Grade src={grades.SS} /></MenuItem>
-        <MenuItem value={'S'}><Grade src={grades.S} /></MenuItem>
-        <MenuItem value={'Ap'}><Grade src={grades.Ap} /></MenuItem>
-        <MenuItem value={'Bp'}><Grade src={grades.Bp} /></MenuItem>
-        <MenuItem value={'Cp'}><Grade src={grades.Cp} /></MenuItem>
-        <MenuItem value={'Dp'}><Grade src={grades.Dp} /></MenuItem>
-        <MenuItem value={'A'}><Grade src={grades.A} /></MenuItem>
-        <MenuItem value={'B'}><Grade src={grades.B} /></MenuItem>
-        <MenuItem value={'C'}><Grade src={grades.C} /></MenuItem>
-        <MenuItem value={'D'}><Grade src={grades.D} /></MenuItem>
-        <MenuItem value={'F'}><Grade src={grades.F} /></MenuItem>
+  return (
+    <Select value={value} label={label} onChange={onChange} defaultValue={"Ap"}>
+      <MenuItem value={"SSS"}>
+        <Grade src={grades.SSS} />
+      </MenuItem>
+      <MenuItem value={"SS"}>
+        <Grade src={grades.SS} />
+      </MenuItem>
+      <MenuItem value={"S"}>
+        <Grade src={grades.S} />
+      </MenuItem>
+      <MenuItem value={"Ap"}>
+        <Grade src={grades.Ap} />
+      </MenuItem>
+      <MenuItem value={"Bp"}>
+        <Grade src={grades.Bp} />
+      </MenuItem>
+      <MenuItem value={"Cp"}>
+        <Grade src={grades.Cp} />
+      </MenuItem>
+      <MenuItem value={"Dp"}>
+        <Grade src={grades.Dp} />
+      </MenuItem>
+      <MenuItem value={"A"}>
+        <Grade src={grades.A} />
+      </MenuItem>
+      <MenuItem value={"B"}>
+        <Grade src={grades.B} />
+      </MenuItem>
+      <MenuItem value={"C"}>
+        <Grade src={grades.C} />
+      </MenuItem>
+      <MenuItem value={"D"}>
+        <Grade src={grades.D} />
+      </MenuItem>
+      <MenuItem value={"F"}>
+        <Grade src={grades.F} />
+      </MenuItem>
     </Select>
-}
+  );
+};
 
-export default GradeDropdown
+export default GradeDropdown;
 
 const Grade = styled.img`
-    height: 35px;
-`
+  height: 35px;
+`;
