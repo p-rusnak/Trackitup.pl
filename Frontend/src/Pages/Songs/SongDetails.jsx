af76676cc1b0a10519d4df67e0fb99695ae9a62c
Paweł <p.j.rusnak97@gmail.com>
2025-07-15 15:43:38 +0200

    Redesign songs page UI

--- Frontend/src/Pages/Songs/SongDetails.jsx
+++ Frontend/src/Pages/Songs/SongDetails.jsx
@@ -1,5 +1,5 @@
-import { Button, TextField } from "@mui/material";
 import React, { useState } from "react";
+import { Box, Typography } from "@mui/material";
 import styled from "styled-components";
 import GradeSelect from "../../Components/GradeSelect";
 import packs from "../../consts/packs";
@@ -7,41 +7,39 @@ import packs from "../../consts/packs";
 const SongDetails = ({ chart, changeGrade }) => {
   const [grade, setGrade] = useState(chart.grade || "");
 
-  chart.diffs.sort((a, b) => {
-    if (a.type > b.type) return -1;
-    if (a.type < b.type) return 1;
-
-    if (a.diff > b.diff) return 1;
-    if (a.diff < b.diff) return -1;
-
-    return 0;
-  });
-
-
   const loggedIn = Boolean(localStorage.getItem("token"));
 
   return (
-    <div>
-      <div>Title: {chart.title}</div>
-      <div>Artist: {chart.artist}</div>
-      <DiffBallMain className={`${chart.mode} ${chart.diff}`} />
-      <div>Packs:</div>
-      {chart.packs.map((pack) => (
-        <div key={pack.id}>
-          - {packs[pack.id]?.name} ({pack.pos}/{packs[pack.id]?.songs})
+    <Box>
+      <Header>
+        <Cover src={chart.img} alt={chart.title} />
+        <div>
+          <Typography variant="h6">{chart.title}</Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {chart.artist}
+          </Typography>
+          <DiffBallMain className={`${chart.mode} ${chart.diff}`} />
         </div>
+      </Header>
+      <Typography variant="subtitle2" sx={{ mt: 2 }}>
+        Packs:
+      </Typography>
+      {chart.packs.map((pack) => (
+        <Typography variant="body2" key={pack.id}>
+          - {packs[pack.id]?.name} ({pack.pos}/{packs[pack.id]?.songs})
+        </Typography>
       ))}
-
-      <div>Other diffs: </div>
+      <Typography variant="subtitle2" sx={{ mt: 2 }}>
+        Other diffs:
+      </Typography>
       <div>
         {chart.diffs.map((d) => (
           <DiffBall
-            key={`${d.type} ${d.diff}`}
+            key={`${d.type}-${d.diff}`}
             className={`${d.type} ${d.diff}`}
           />
         ))}
       </div>
-
       {loggedIn && (
         <GradeSelect
           label="Set Grade"
@@ -52,24 +50,31 @@ const SongDetails = ({ chart, changeGrade }) => {
           }}
         />
       )}
-      
-      {/* TODO: score upload */}
-    </div>
+    </Box>
   );
 };
 
 export default SongDetails;
 
+const Header = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Cover = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+`;
+
 const DiffBallMain = styled.span`
   display: inline-block;
   width: 80px;
   height: 80px;
-  position: absolute;
-  right: 20px;
-  top: 20px;
 `;
+
 const DiffBall = styled.span`
   display: inline-block;
-  width: 50px;
-  height: 50px;
+  width: 40px;
+  height: 40px;
 `;
