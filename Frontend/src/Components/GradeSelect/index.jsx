778bd4f3d9286c4ccc6ffb3d2899c413d9d7ef1f
Paweł <p.j.rusnak97@gmail.com>
2025-07-17 11:14:50 +0200

    Reduce grade select icon size

--- Frontend/src/Components/GradeSelect/index.jsx
+++ Frontend/src/Components/GradeSelect/index.jsx
@@ -37,7 +37,7 @@ const GradeSelect = ({ value, label, onChange }) => {
 export default GradeSelect
 
 const Grade = styled.img`
-    height: 35px;
+    height: 30px;
 `
 
 const Wrapper = styled.div`
